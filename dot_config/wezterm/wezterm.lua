7368a5f192784b44c8357f3903e64bd001ab9051
Steven Byrnes <steven@byrnes.org>
2024-08-21 12:05:33 -0500

    adding wezterm initial config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,39 @@
+local wezterm = require 'wezterm'
+return {
+	-- color_scheme = 'termnial.sexy',
+	color_scheme = 'Catppuccin Mocha',
+	enable_tab_bar = false,
+	font_size = 16.0,
+	font = wezterm.font('JetBrainsMono Nerd Font'),
+	-- macos_window_background_blur = 40,
+	macos_window_background_blur = 30,
+
+	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
+	-- window_background_image_hsb = {
+	-- 	brightness = 0.01,
+	-- 	hue = 1.0,
+	-- 	saturation = 0.5,
+	-- },
+	window_background_opacity = 0.85,
+	window_decorations = 'RESIZE',
+	keys = {
+		{
+			key = 'f',
+			mods = 'CMD',
+			action = wezterm.action.ToggleFullScreen,
+		},
+		--{
+		--	key = '\'',
+		--	mods = 'CTRL',
+		--	action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+		--},
+	},
+	--mouse_bindings = {
+	--  -- Ctrl-click will open the link under the mouse cursor
+	--  {
+	--    event = { Up = { streak = 1, button = 'Left' } },
+	--    mods = 'CTRL',
+	--    action = wezterm.action.OpenLinkAtMouseCursor,
+	--  },
+	--},
+}
