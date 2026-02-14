816ac0e3cea1bdf3b8059f1f719069401c1701aa
Steven B <erewhon@flatland.org>
2026-02-14 14:09:55 -0600

    Fix diagnostic dim highlights to target virtual_lines groups

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,12 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Dim diagnostic virtual text and only show on the current line
+-- Only show diagnostic virtual lines on the current line, dimmed
 vim.diagnostic.config({
 	virtual_text = false,
 	virtual_lines = { current_line = true },
 })
 
-vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { fg = "#5a524c" })
-vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { fg = "#5a524c" })
-vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { fg = "#5a524c" })
+local dim = "#3a3530"
+vim.api.nvim_set_hl(0, "DiagnosticVirtualLinesWarn", { fg = dim })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualLinesHint", { fg = dim })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualLinesInfo", { fg = dim })
+vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = dim })
+vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = dim })
+vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = dim })
