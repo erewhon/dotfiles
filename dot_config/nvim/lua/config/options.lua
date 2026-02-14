b6443295e44eb7928b0c446a1bb45a4885e84349
Steven B <erewhon@flatland.org>
2026-02-14 14:06:03 -0600

    Dim diagnostic virtual text and show only on current line

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Dim diagnostic virtual text and only show on the current line
+vim.diagnostic.config({
+	virtual_text = false,
+	virtual_lines = { current_line = true },
+})
+
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { fg = "#5a524c" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { fg = "#5a524c" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { fg = "#5a524c" })
