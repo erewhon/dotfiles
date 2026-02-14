7e8278c4ef87b0b456c9dc3bc87edb64014668e8
Steven B <erewhon@flatland.org>
2026-02-14 12:06:34 -0600

    Add eye candy nvim plugins: rainbow-delimiters, fidget, cellular-automaton, colorizer, reactive, twilight, zen-mode, satellite

--- dot_config/nvim/lua/plugins/erewhon.lua
+++ dot_config/nvim/lua/plugins/erewhon.lua
@@ -4,18 +4,82 @@ return {
 		event = "VeryLazy",
 		ft = { "org" },
 		config = function()
-			-- Setup orgmode
 			require("orgmode").setup({
 				org_agenda_files = "~/orgfiles/**/*",
 				org_default_notes_file = "~/orgfiles/refile.org",
 			})
-
-			-- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-			-- add ~org~ to ignore_install
-			-- require('nvim-treesitter.configs').setup({
-			--   ensure_installed = 'all',
-			--   ignore_install = { 'org' },
-			-- })
 		end,
+	},
+
+	-- Rainbow-colored matching brackets/parens
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		event = "BufReadPost",
+	},
+
+	-- LSP progress spinners
+	{
+		"j-hui/fidget.nvim",
+		event = "LspAttach",
+		opts = {},
+	},
+
+	-- Make your code dissolve (Game of Life) or rain down
+	{
+		"Eandrju/cellular-automaton.nvim",
+		keys = {
+			{ "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "Make it rain" },
+			{ "<leader>gol", "<cmd>CellularAutomaton game_of_life<CR>", desc = "Game of Life" },
+		},
+	},
+
+	-- Inline color previews for hex codes, CSS, etc.
+	{
+		"NvChad/nvim-colorizer.lua",
+		event = "BufReadPost",
+		opts = {
+			user_default_options = {
+				names = false,
+				css = true,
+				tailwind = true,
+			},
+		},
+	},
+
+	-- Cursorline/signcolumn changes color based on mode
+	{
+		"rasulomarern/reactive.nvim",
+		event = "VeryLazy",
+		opts = {
+			load = { "catppuccin-mocha-cursor", "catppuccin-mocha-cursorline" },
+		},
+	},
+
+	-- Dim inactive code blocks
+	{
+		"folke/twilight.nvim",
+		cmd = { "Twilight", "TwilightEnable" },
+		opts = {},
+	},
+
+	-- Distraction-free coding mode
+	{
+		"folke/zen-mode.nvim",
+		cmd = "ZenMode",
+		keys = {
+			{ "<leader>z", "<cmd>ZenMode<CR>", desc = "Zen Mode" },
+		},
+		opts = {
+			plugins = {
+				twilight = { enabled = true },
+			},
+		},
+	},
+
+	-- Scrollbar with diagnostic/git/search markers
+	{
+		"lewis6991/satellite.nvim",
+		event = "BufReadPost",
+		opts = {},
 	},
 }
